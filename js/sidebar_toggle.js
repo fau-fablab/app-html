74b1de234522a8e9be7a01585496529e474a3d24
Stefan Herpich <stefan.herpich@t-online.de>
2015-07-20 14:20:03 +0200

    Created first design approach with bootstrap used plain js (typescript version will follow)

---
+++ js/sidebar_toggle.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var hamburger = $("#menu-toggle");
+    var overlay = $(".overlay");
+    
+	$('.dropdown').on('show.bs.dropdown', function(e){
+	    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(300);
+	});
+	$('.dropdown').on('hide.bs.dropdown', function(e){
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp(300);
+	});
+	hamburger.click(function(e) {
+		e.preventDefault();
+		var elem = document.getElementById("sidebar-wrapper");
+		left = window.getComputedStyle(elem,null).getPropertyValue("left");
+		if(left == "200px"){
+			document.getElementsByClassName("sidebar-toggle")[0].style.left="-200px";
+			overlay.hide();
+		}
+		else if(left == "-200px"){
+			document.getElementsByClassName("sidebar-toggle")[0].style.left="200px";
+			overlay.show();
+		}
+	});
+	
+	
+});
