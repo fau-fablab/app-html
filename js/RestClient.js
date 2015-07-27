52820cd768e8d7a496b7aedd49720c8cf00974ae
Stefan Herpich <Stefan.Herpich@t-online.de>
2015-07-27 10:07:01 +0200

    forgot some typescript/javascript files to commit

---
+++ js/RestClient.js
@@ -0,0 +1,46 @@
+// typescript import to create XDomainRequests
+/// <reference path="lib.d.ts" />
+// General REST class
+var RestClient = (function () {
+    function RestClient() {
+        this.url = "https://ec2-52-28-16-59.eu-central-1.compute.amazonaws.com";
+    }
+    // CORS request
+    RestClient.prototype.request = function (method, path, callback) {
+        var urlPath = this.url + path;
+        var xhr = this.createCORSRequest(method, urlPath);
+        if (!xhr) {
+            alert('CORS not supported');
+            return null;
+        }
+        // return json response and handle reponse in the specific callback function
+        xhr.onload = function () {
+            var response = JSON.parse(xhr.responseText);
+            callback(response);
+        };
+        xhr.onerror = function () {
+            alert('An error occured while loading the content.');
+            return null;
+        };
+        xhr.send();
+    };
+    // Create the XHR object
+    RestClient.prototype.createCORSRequest = function (method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+            // XHR for Chrome/Firefox/Opera/Safari.
+            xhr.open(method, url, true);
+        }
+        else if (typeof XDomainRequest != "undefined") {
+            // XDomainRequest for IE
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+        }
+        else {
+            // CORS not supported.
+            xhr = null;
+        }
+        return xhr;
+    };
+    return RestClient;
+})();
---
+++ js/news_and_ical.js
@@ -0,0 +1,16 @@
+/// <reference path="RestClient.ts"/>
+/// <reference path="jquery.d.ts" />
+// import RestClient.js in order to use it and append this js
+$.getScript("js/RestClient.js", function () {
+    // make CORS REST calls by using class RestClient
+    var client = new RestClient();
+    client.request("GET", "/news/all", addNews);
+    function addNews(news) {
+        for (var i = 0; i < news.length; i++) {
+            $("#news_container").append("<div class='card'>" +
+                "<div class=\"card-image\" style=\"background-image:url('" + news[i].linkToPreviewImage + "');\"/>" +
+                "<h2>" + news[i].title + "</h2>" +
+                "<p>" + news[i].descriptionShort + "</p></div>");
+        }
+    }
+});
