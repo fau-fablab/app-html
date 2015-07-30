8a958af6e2208e7af61de41b5d7c3be1fe3a3989
Stefan Herpich <Stefan.Herpich@t-online.de>
2015-07-30 11:26:08 +0200

    added default news picture when noone is available and added data to each of the news divs to handle a popup event

--- js/news_and_ical.js
+++ js/news_and_ical.js
@@ -5,12 +5,12 @@ $.getScript("js/RestClient.js", function () {
     // make CORS REST calls by using class RestClient
     var client = new RestClient();
     client.request("GET", "/news/all", addNews);
+    // callback function to add news to the news_container
     function addNews(news) {
         for (var i = 0; i < news.length; i++) {
-            $("#news_container").append("<div class='card'>" +
-                "<div class=\"card-image\" style=\"background-image:url('" + news[i].linkToPreviewImage + "');\"/>" +
-                "<h2>" + news[i].title + "</h2>" +
-                "<p>" + news[i].descriptionShort + "</p></div>");
+            var image = news[i].linkToPreviewImage || "img/news_nopicture.png";
+            $("#news_container").append("<div class='card' data-image='" + image + "' data-title='" + news[i].title + "' data-descriptionShort='" + news[i].descriptionShort + "'>" + "<div class=\"card-image\" style=\"background-image:url('" + image + "');\"/>" + "<h2>" + news[i].title + "</h2>" + "<p>" + news[i].descriptionShort + "</p></div>");
         }
     }
 });
+//# sourceMappingURL=news_and_ical.js.map
--- js/news_and_ical.ts
+++ js/news_and_ical.ts
@@ -1,17 +1,21 @@
 /// <reference path="RestClient.ts"/>
-﻿/// <reference path="jquery.d.ts" />
+/// <reference path="jquery.d.ts" />
 
 // import RestClient.js in order to use it and append this js
 $.getScript("js/RestClient.js", function(){
 	// make CORS REST calls by using class RestClient
 	var client:RestClient = new RestClient();
 	client.request("GET","/news/all", addNews);
-	function addNews(news){
-		for(var i=0; i< news.length;i++){
-			$("#news_container").append("<div class='card'>" +
-					"<div class=\"card-image\" style=\"background-image:url('"+ news[i].linkToPreviewImage +"');\"/>" +
-					"<h2>"+ news[i].title +"</h2>" +
-					"<p>"+ news[i].descriptionShort +"</p></div>"); 
+
+    // callback function to add news to the news_container
+	function addNews(news) {
+		for (var i = 0; i < news.length; i++) {
+            var image = news[i].linkToPreviewImage || "img/news_nopicture.png";
+			$("#news_container").append("<div class='card' data-image='"+ image +"' data-title='"+ news[i].title +"' data-descriptionShort='"+ news[i].descriptionShort +"'>" +
+				"<div class=\"card-image\" style=\"background-image:url('" + image + "');\"/>" +
+				"<h2>" + news[i].title + "</h2>" +
+				"<p>" + news[i].descriptionShort + "</p></div>");
 		}
 	}
+
 });
