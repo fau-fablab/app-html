caf4ef95f372e30c0ff37094b7cadf4a6a67393c
johannes <jopf88@googlemail.com>
2015-08-13 13:47:08 +0200

    - Suchfeld mit der Suche nach ID, Namen und nach allen Produkten ist implementiert - Der erste Entwurf für das Suchergebnis ist fertig

--- js/product_search.ts
+++ js/product_search.ts
@@ -5,20 +5,74 @@
 // import RestClient.js in order to use it and append this js
 $.getScript("js/RestClient.js", function(){
 
+});
 
-    var LOADLIMIT = 2;
+function sayHallo(){
+    alert("asdfaf")
+}
+
+function search(){
+    var restClient = new RestClient();
+    var LOADLIMIT = 50;
     var OFFSET = 0;
+    var researchCriteria = $('#inputSuche').val();
+    console.log(researchCriteria);
 
-    var client:RestClient = new RestClient();
+    hideEmptyResultText();
 
-    client.request("GET","/products?offset="+OFFSET+"&limit="+LOADLIMIT, showProducts);
-
-
-    function showProducts(records){
-        for (var i = 0; i < records.length; i++) {
-            $("#seach_results").append("<div> <h1> " + records[i].name + " " + records[i].location +  " </h1></div>");
-        }
+    if(researchCriteria == ""){
+        console.log("war leer");
+        restClient.request("GET","/products?offset="+OFFSET+"&limit="+LOADLIMIT, showProducts);
+    }
+    else if(isNumber(researchCriteria)){
+        console.log("War number");
+        restClient.request("GET","/products/find/id?id="+researchCriteria, showProducts);
+    }
+    else{
+        console.log("War ein String");
+        restClient.request("GET","/products/find/name?search="+researchCriteria+"&limit="+LOADLIMIT+"&offset="+OFFSET, showProducts);
     }
 
 
-});
+}
+
+function showProducts(records){
+    cleanTable();
+
+    if(records.length == 0){
+        console.log("Records waren leer");
+        showEmptyResultText();
+    }
+    for (var i = 0; i < records.length; i++) {
+        $("#search_results").append("<tr>" +
+            " <td>"+ records[i].productId+"</td>" +
+            " <td><div>"+ records[i].name+"</div><div>"+ records[i].category.name+"</div></td>" +
+            " <td>"+ records[i].location+"</td>" +
+            " <td><div>"+ records[i].price+" <span class=\"glyphicon glyphicon-euro\"></span></div><div>"+ records[i].uom.name+"</div></td>" +
+            "</tr>");
+    }
+}
+
+function isNumber(value: String){
+    for(var index = 0; index < value.length; index++){
+        if(value[index].search(/[0-9]/) == -1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function cleanTable(){
+    console.log("clickt cleanTable");
+    $("#search_results").empty();
+}
+
+function showEmptyResultText(){
+    var headline = $("#empty_text").show();
+    headline.text("Keine Treffer!");
+}
+
+function hideEmptyResultText(){
+    $("#empty_text").hide();
+}
+
