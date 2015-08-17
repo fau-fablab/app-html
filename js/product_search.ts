39cee83496aa514ee8ccee225e015da12902bb1e
johannes <jopf88@googlemail.com>
2015-08-17 13:03:30 +0200

    - Modified product_search.ts with some sort-algo to sort the table (still working)

--- js/product_search.ts
+++ js/product_search.ts
@@ -4,65 +4,58 @@
 /// <reference path="common/model/Category.ts" />
 /// <reference path="common/model/Product.ts" />
 
-var currentProcutList: Array<common.Product> = new Array<common.Product>();
+var currentProcutList:Array<common.Product> = new Array<common.Product>();
 
-function search(): void{
-    console.log("Start 08");
+function search():void {
+
     var restClient = new RestClient();
-    var LOADLIMIT: number = 0;
-    var OFFSET: number = 0;
-    var researchCriteria: any = $('#inputSuche').val();
+    var LOADLIMIT:number = 50;
+    var OFFSET:number = 0;
+    var researchCriteria:any = $('#inputSuche').val();
 
     hideEmptyResultText();
 
 
-    if(researchCriteria == ""){
-        restClient.request("GET","/products?offset="+OFFSET+"&limit="+LOADLIMIT, showProducts);
+    if (researchCriteria == "") {
+        restClient.request("GET", "/products?offset=" + OFFSET + "&limit=" + LOADLIMIT, showProducts);
     }
-    else if(isNumber(researchCriteria)){
-        restClient.request("GET","/products/find/id?id="+researchCriteria, showProduct);
+    else if (isNumber(researchCriteria)) {
+        restClient.request("GET", "/products/find/id?id=" + researchCriteria, showProduct);
     }
-    else{
-        restClient.request("GET","/products/find/name?search="+researchCriteria+"&limit="+LOADLIMIT+"&offset="+OFFSET, showProducts);
+    else {
+        restClient.request("GET", "/products/find/name?search=" + researchCriteria + "&limit=" + LOADLIMIT + "&offset=" + OFFSET, showProducts);
     }
 
 }
-function showProduct(record:any): void{
+function showProduct(record:any):void {
     var recordArray = [];
     recordArray.push(record);
     showProducts(recordArray);
 }
 
-function showProducts(records: any): void{
+function showProducts(records:any):void {
     cleanTable();
+    currentProcutList.length = 0;
     console.log(records);
-    if(records.length == 0){
+    if (records.length == 0) {
         console.log("Records waren leer");
         showEmptyResultText();
     }
     for (var index = 0; index < records.length; index++) {
         var product = new common.Product(records[index]);
         currentProcutList.push(product);
-        // probleme mit
-        var categoryName: string= product.categoryObject.name;
-        var uomName: string = product.uomObject.name;
-
-
-        $("#search_results").append("<tr data-toggle='modal' data-target='#myModal' class='product_row' productid='"+ product.productId +"' arrayindex='"+index+"'> " +
-            " <td id='productId' '>"+ product.productId+"</td>" +
-            " <td id='productName'><div>"+ product.name+"</div><div>"+ categoryName+"</div></td>" +
-            " <td id='productLocation'>"+ product.locationString+"</td>" +
-            " <td id='productPrice'><div>"+ product.price+" <span class=\"glyphicon glyphicon-euro\"></span></div><div>"+ uomName+"</div></td>" +
-            "</tr>");
     }
+    buildTableRows(currentProcutList);
+    prepareDialogFunktions();
+}
 
-
-    $(".product_row").click(function(event){
+function prepareDialogFunktions() {
+    $(".product_row").click(function (event) {
 
         var currentElement = $(this);
         var productId = currentElement.attr("productid");
         var arrayIndex = currentElement.attr("arrayindex");
-        var currentProduct: common.Product = currentProcutList[arrayIndex];
+        var currentProduct:common.Product = currentProcutList[arrayIndex];
         console.log("ProductId: " + productId);
         console.log("ArrayIndex: " + arrayIndex);
         console.log("ProductId: " + currentProcutList[arrayIndex]._productId)
@@ -77,7 +70,7 @@ function showProducts(records: any): void{
         var modalProductCategoryLabel = $("#modal-productCategory");
 
         modalHeaderName.text(currentProduct.name);
-        modalProductIdLabel.text(currentProduct.productId+"");
+        modalProductIdLabel.text(currentProduct.productId + "");
         modalProductNameLabel.text(currentProduct.name);
         modalProductDescriptionLabel.text(currentProduct.description);
         modalProductPriceLabel.text(currentProduct.price + " \u20AC");
@@ -88,51 +81,164 @@ function showProducts(records: any): void{
     });
 }
 
-function createTableHeader(){
+function buildTableRows(productArray:Array<common.Product>) {
+    cleanTable();
+    for (var index = 0; index < productArray.length; index++) {
+        var product = productArray[index];
+        var categoryName:string = product.categoryObject.name;
+        var uomName:string = product.uomObject.name;
+
+        $("#search_results").append("<tr data-toggle='modal' data-target='#myModal' class='product_row' productid='" + product.productId + "' arrayindex='" + index + "'> " +
+            " <td id='productId' '>" + product.productId + "</td>" +
+            " <td id='productName'><div>" + product.name + "</div><div>" + categoryName + "</div></td>" +
+            " <td id='productLocation'>" + product.locationString + "</td>" +
+            " <td id='productPrice'><div>" + product.price + " <span class=\"glyphicon glyphicon-euro\"></span></div><div>" + uomName + "</div></td>" +
+            "</tr>");
+    }
+    prepareDialogFunktions();
+}
+
+function createTableHeader() {
     $("#search_results").append("<tr> " +
-            " <th onclick='sortById()'>"+"Id"+"</th>" +
-            " <th onclick='sortByName()'>"+"Name"+"</th>" +
-            " <th onclick='sortByLocation()'>"+"Lagerort"+"</th>" +
-            " <th onclick='sortByPrice()'>"+"Preis"+"</th>" +
+        " <th onclick='sortById()'>" + "Id" + "</th>" +
+        " <th onclick='sortByName()'>" + "Name" + "</th>" +
+        " <th onclick='sortByLocation()'>" + "Lagerort" + "</th>" +
+        " <th onclick='sortByPrice()'>" + "Preis" + "</th>" +
         "</tr>");
 }
 
-function isNumber(value: String): boolean{
-    for(var index = 0; index < value.length; index++){
-        if(value[index].search(/[0-9]/) == -1) {
+function isNumber(value:String):boolean {
+    for (var index = 0; index < value.length; index++) {
+        if (value[index].search(/[0-9]/) == -1) {
             return false;
         }
     }
     return true;
 }
 
-function cleanTable(): void{
-    currentProcutList.length = 0;
+function cleanTable():void {
     $("#search_results").empty();
     createTableHeader();
 }
 
-function showEmptyResultText(): void{
+function showEmptyResultText():void {
     var headline = $("#empty_text").show();
     headline.text("Keine Treffer!");
 }
 
-function hideEmptyResultText(): void{
+function hideEmptyResultText():void {
     $("#empty_text").hide();
 }
 
-function sortById(){
+function sortById() {
     console.log("sortByID");
+    var newArrayAscendingOrder = new Array<common.Product>();
+    //var newArrayDescendingOrder = new Array<common.Product>();
+
+    newArrayAscendingOrder = currentProcutList;
+
+    var tempProduct:common.Product = null;
+    //Von klein nach groﬂ
+    for (var index = 0; index < newArrayAscendingOrder.length - 1; index++) {
+        for (var innerIndex = index + 1; innerIndex < newArrayAscendingOrder.length - 1; innerIndex++) {
+            console.log("Vergleiche: " + newArrayAscendingOrder[innerIndex].productId + " und " + newArrayAscendingOrder[innerIndex + 1].productId)
+            if (newArrayAscendingOrder[innerIndex].productId > newArrayAscendingOrder[innerIndex + 1].productId) {
+                tempProduct = newArrayAscendingOrder[innerIndex];
+                newArrayAscendingOrder[innerIndex] = newArrayAscendingOrder[innerIndex + 1];
+                newArrayAscendingOrder[innerIndex + 1] = tempProduct;
+            }
+        }
+
+
+    }
+    for (var newindex = 0; newindex < newArrayAscendingOrder.length; newindex++) {
+        console.log(newArrayAscendingOrder[newindex].productId);
+    }
+    buildTableRows(newArrayAscendingOrder);
 }
 
-function sortByName(){
+function sortByName() {
     console.log("sortByName");
+    newArrayAscendingOrder: Array<common.Product>();
+    var newArrayAscendingOrder = new Array<common.Product>();
+    //var newArrayDescendingOrder = new Array<common.Product>();
+
+    newArrayAscendingOrder = currentProcutList;
+
+
+    var tempProduct:common.Product = null;
+    //Von klein nach groﬂ
+    var bla = 0;
+
+    bla++;
+    for (var index = 0; index < newArrayAscendingOrder.length - 1; index++) {
+        for (var innerIndex = index + 1; innerIndex < newArrayAscendingOrder.length - 1; innerIndex++) {
+            if ((newArrayAscendingOrder[innerIndex].name[0]) > (newArrayAscendingOrder[innerIndex + 1].name[0])) {
+                tempProduct = newArrayAscendingOrder[innerIndex];
+                newArrayAscendingOrder[innerIndex] = newArrayAscendingOrder[innerIndex + 1];
+                newArrayAscendingOrder[innerIndex + 1] = tempProduct;
+            }
+        }
+    }
+
+    for (var newindex = 0; newindex < newArrayAscendingOrder.length; newindex++) {
+        console.log(newArrayAscendingOrder[newindex].locationString[0]);
+    }
+
+    buildTableRows(newArrayAscendingOrder);
 }
 
-function sortByLocation(){
+function sortByLocation() {
     console.log("sortByLocation");
+    newArrayAscendingOrder: Array<common.Product>();
+    var newArrayAscendingOrder = new Array<common.Product>();
+    //var newArrayDescendingOrder = new Array<common.Product>();
+
+    newArrayAscendingOrder = currentProcutList;
+
+
+    var tempProduct:common.Product = null;
+    //Von klein nach groﬂ
+    for (var index = 0; index < newArrayAscendingOrder.length - 1; index++) {
+        for (var innerIndex = index + 1; innerIndex < newArrayAscendingOrder.length - 1; innerIndex++) {
+            if ((newArrayAscendingOrder[innerIndex].locationString[0]) > (newArrayAscendingOrder[innerIndex + 1].locationString[0])) {
+                tempProduct = newArrayAscendingOrder[innerIndex];
+                newArrayAscendingOrder[innerIndex] = newArrayAscendingOrder[innerIndex + 1];
+                newArrayAscendingOrder[innerIndex + 1] = tempProduct;
+            }
+        }
+    }
+    for (var newindex = 0; newindex < newArrayAscendingOrder.length; newindex++) {
+        console.log(newArrayAscendingOrder[newindex].locationString[0]);
+    }
+    buildTableRows(newArrayAscendingOrder);
 }
 
-function sortByPrice(){
+function sortByPrice() {
     console.log("sortByPrice");
+    var newArrayAscendingOrder = new Array<common.Product>();
+    //var newArrayDescendingOrder = new Array<common.Product>();
+
+    newArrayAscendingOrder = currentProcutList;
+
+    var tempProduct:common.Product = null;
+    //Von klein nach groﬂ
+    for (var index = 0; index < newArrayAscendingOrder.length - 1; index++) {
+        for (var innerIndex = index + 1; innerIndex < newArrayAscendingOrder.length - 1; innerIndex++) {
+            console.log("Vergleiche: " + (newArrayAscendingOrder[innerIndex].price * 1000) + " und " + (newArrayAscendingOrder[innerIndex + 1].price + 1000))
+            if ((newArrayAscendingOrder[innerIndex].price * 1000) > (newArrayAscendingOrder[innerIndex + 1].price * 1000)) {
+                tempProduct = newArrayAscendingOrder[innerIndex];
+                newArrayAscendingOrder[innerIndex] = newArrayAscendingOrder[innerIndex + 1];
+                newArrayAscendingOrder[innerIndex + 1] = tempProduct;
+            }
+        }
+    }
+    for (var newindex = 0; newindex < newArrayAscendingOrder.length; newindex++) {
+        console.log(newArrayAscendingOrder[newindex].price);
+    }
+    buildTableRows(newArrayAscendingOrder);
+}
+
+function startup():void {
+    console.log("Start 08");
 }
