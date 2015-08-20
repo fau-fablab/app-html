833a1cae629d867d488f5da103c9ab898ecde3a5
johannes <jopf88@googlemail.com>
2015-08-20 12:45:49 +0200

    - Modified Dialog with new Spinner

--- js/product_search.ts
+++ js/product_search.ts
@@ -218,3 +218,14 @@ function sortByPrice() {
     createTableRows(newArrayAscendingOrder);
 
 }
+
+(function ($) {
+    $('.spinner .btn:first-of-type').on('click', function() {
+        $('.spinner input').val( parseInt($('.spinner input').val(), 10) + 1);
+    });
+    $('.spinner .btn:last-of-type').on('click', function() {
+        $('.spinner input').val( parseInt($('.spinner input').val(), 10) - 1);
+    });
+})(jQuery);
+
+
