86d19961a90221f25d1affd9de49fccaced8c2da
johannes <jopf88@googlemail.com>
2015-08-20 12:52:56 +0200

    - Modified Dialog with new Spinner - fixed some stuff

--- js/product_search.ts
+++ js/product_search.ts
@@ -219,13 +219,3 @@ function sortByPrice() {
 
 }
 
-(function ($) {
-    $('.spinner .btn:first-of-type').on('click', function() {
-        $('.spinner input').val( parseInt($('.spinner input').val(), 10) + 1);
-    });
-    $('.spinner .btn:last-of-type').on('click', function() {
-        $('.spinner input').val( parseInt($('.spinner input').val(), 10) - 1);
-    });
-})(jQuery);
-
-
