fa227dfcd1c7e869aaebeda7eeb5ad3e2794fd79
johannes <jopf88@googlemail.com>
2015-08-22 22:55:15 +0200

    - Modified für Stefan

--- js/RestClient.ts
+++ js/RestClient.ts
@@ -5,7 +5,8 @@ class RestClient{
 	private _url:string;
 
 	constructor(){
-		this._url = "https://ec2-52-28-16-59.eu-central-1.compute.amazonaws.com:4433";
+		//this._url = "https://ec2-52-28-16-59.eu-central-1.compute.amazonaws.com:4433";
+		this._url = "http://192.168.2.102:8080"
 	}
 	
 	// CORS request
@@ -36,7 +37,6 @@ class RestClient{
 		  }
         }
 
-
 	// Create the XHR object
 	createCORSRequest(method:string,url:string):XMLHttpRequest{
 	  	  var xhr:XMLHttpRequest = new XMLHttpRequest();
@@ -52,5 +52,27 @@ class RestClient{
 		    xhr = null;
 		  }
 		  return xhr;
+	}
+
+	postRequest(message:Object, path:string, callback: (s: string) => any, param?:string):void{
+		var urlPath:string = this._url + path;
+
+		var xhr:XMLHttpRequest = this.createCORSRequest("POST", urlPath);
+		if (!xhr) {
+			alert('CORS not supported');
+			return null;
+		}
+
+		// return json response and handle response in the specific callback function
+		xhr.onload = function() {
+			var response:string = JSON.parse(xhr.responseText);
+			callback(response);
+		};
+
+		xhr.onerror = function() {
+			alert('An error occured while loading the content.');
+			return null;
+		};
+		xhr.send(message);
 	}
 }
--- js/contact.ts
+++ js/contact.ts
@@ -1,7 +1,10 @@
 /// <reference path="jquery.d.ts" />
+/// <reference path="RestClient.ts"/>
+/// <reference path="common/model/Product.ts" />
 
 function sendMail(){
     console.log("Sende Mail");
+    var restClient: RestClient = new RestClient();
 
     var emailInput: any = $("#contact_email");
     var c: any = $("#contact_message");
@@ -9,13 +12,23 @@ function sendMail(){
     var email: string = emailInput.val();
     var message: string = emailInput.val();
 
-
-
     console.log(email);
     console.log(message);
 
-    $("#contact_email").val("");
+    var value: any = "{bla: 4}"
 
+
+
+    var testObject = {"first":4,"second":"asdf"};
+    var testObject2 = {"first":2,"second":"asdfa"};
+    restClient.postRequest(testObject2,"/mail/test",callback);
+
+    $("#contact_email").val("");
     $("#contact_message").val("");
 
+}
+
+function callback(value:any):void{
+    console.log("In Callack");
+    console.log("ReturnValue: " + value);
 }
