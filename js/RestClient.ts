0e935a410defd83f1144582e726ba0014c67aa9e
johannes <jopf88@googlemail.com>
2015-08-25 23:48:21 +0200

    - Deleted local ip

--- js/RestClient.ts
+++ js/RestClient.ts
@@ -5,8 +5,7 @@ class RestClient{
 	private _url:string;
 
 	constructor(){
-		//this._url = "https://ec2-52-28-16-59.eu-central-1.compute.amazonaws.com:4433";
-		this._url = "http://192.168.2.102:8080"
+		this._url = "https://ec2-52-28-16-59.eu-central-1.compute.amazonaws.com:4433";
 	}
 	
 	// CORS request
