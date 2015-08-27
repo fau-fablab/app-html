1c56b69e04ae00019f1c7e321ec1d8bff4497867
johannes <jopf88@googlemail.com>
2015-08-27 15:29:49 +0200

    - Deleted functions to test my api

--- js/app.ts
+++ js/app.ts
@@ -28,38 +28,7 @@ function triggerDoorStateUpdate() {
 }
 
 $(document).ready(function () {
-    var productApi: ProductApi = new ProductApi();
-    productApi.findAll(5,0,findAllProductCallback);
-    productApi.findById("0008",findProductByIdCallback);
-    productApi.findByName("Holzschraube",10,0,findProductByNameCallback);
-    productApi.findByCategory("1-reihig",10,0,findProductByCategoryCallback);
-    productApi.findAllNames(findProductNamesCallBack);
-
     spaceapi = new SpaceApi("FAU+FabLab", updateDoorState);
     $("#doorState").click(triggerDoorStateUpdate);
 });
 
-function findAllProductCallback(records){
-    console.log("FindAll");
-    console.log(records);
-}
-
-function findProductByIdCallback(records){
-    console.log("FindByID");
-    console.log(records);
-}
-
-function findProductByNameCallback(records){
-    console.log("FindByName");
-    console.log(records);
-}
-
-function findProductByCategoryCallback(records){
-    console.log("FindByCategory");
-    console.log(records);
-}
-
-function findProductNamesCallBack(records){
-    console.log("Find ProductNames");
-    console.log(records);
-}
