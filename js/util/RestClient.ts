a8dd196702485ee8ed8a9be7500c1412a4e9c10d
Philip Kanhäußer <git@mail.philip.xers.de>
2015-09-01 12:26:55 +0200

    Updated url to new dev server Fixed duplicate function names.

--- js/util/RestClient.ts
+++ js/util/RestClient.ts
@@ -7,7 +7,7 @@ class RestClient{
 	private _authentication:string;
 
 	constructor(){
-		this._url = "https://ec2-52-28-16-59.eu-central-1.compute.amazonaws.com:4433";
+		this._url = "https://ec2-52-28-163-255.eu-central-1.compute.amazonaws.com:4433";
 		this._authentication = "";
 	}
 	
