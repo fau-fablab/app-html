f230eec0c6f6b3f750c2d911d40e51d190e6b8a7
johannes <jopf88@googlemail.com>
2015-10-12 16:05:56 +0200

    - Problem mit den Umlauten gel\u00f6st

--- js/elements/InfoResource.ts
+++ js/elements/InfoResource.ts
@@ -1,21 +1,21 @@
 class InfoResource{
 
     public productInfo():string{
-        return "Gib das gewünschte Produkt ein und drücke auf Suchen. Wenn du alle Produkte finden willst, " +
-        "dann lasse das Feld frei und suche direkt. Alternativ kannst du auf 'Erweiterte Suche' drücken um weitere Suchoptionen zu erhalten " +
+        return "Gib das gew\u00fcnschte Produkt ein und dr\u00fccke auf Suchen. Wenn du alle Produkte finden willst, " +
+        "dann lasse das Feld frei und suche direkt. Alternativ kannst du auf 'Erweiterte Suche' dr\u00fccken um weitere Suchoptionen zu erhalten " +
         "z.B. um Produkte gezielt nach Kategorien zu suchen oder um dir alle Kategorien mit den Produkten anzeigen zu lassen.";
     }
 
     public cartInfo(): string{
-        return "Wenn du bezahlen möchtest, generiere dir an der Kasse im FABLAB einen QR Code " +
+        return "Wenn du bezahlen m\u00f6chtest, generiere dir an der Kasse im FABLAB einen QR Code " +
             "und klicke hier auf den \"Zur Kasse\"-Button. Nun musst du den QR Code abtippen, auf \"Absenden\" klicken " +
-            "und anschließend bequem im FABLAB bezahlen.";
+            "und danach bequem im FABLAB bezahlen.";
     }
 
     public reservationInfo(): string{
-        return "Wähle eine Maschine aus dem Dropdown Menü aus, " +
-            "fülle anschließend die drei gegebenen Felder entsprechend aus und trage dich per Klick in die Warteliste ein. " +
-            "Du sieht anschließend die aktuelle Warteschlange und deine Position. Wenn du doch nicht mehr warten willst oder keine Zeit mehr hast," +
+        return "W\u00e4hle eine Maschine aus dem Dropdown Men\u00fc aus, " +
+            "f\u00fclle danach die drei gegebenen Felder entsprechend aus und trage dich per Klick in die Warteliste ein. " +
+            "Du sieht anschlie\u00dfend die aktuelle Warteschlange und deine Position. Wenn du doch nicht mehr warten willst oder keine Zeit mehr hast," +
             " trage dich bitte wieder aus indem du auf das kleine X in deiner Zeile klickst.";
     }
 
